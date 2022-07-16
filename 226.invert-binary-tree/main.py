430bb993fb42677bd6bbb931570aa39b270ac26c
Nahum Litvin <lnahum@gmail.com>
2022-07-16 11:05:41 +0300

    sat16th - this one was actually easy!

---
+++ 226.invert-binary-tree/main.py
@@ -0,0 +1,16 @@
+from operator import inv
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        tmp= self.invertTree(root.right)
+        root.right  = self.invertTree(root.left)
+        root.left = tmp
+        return root
+             
