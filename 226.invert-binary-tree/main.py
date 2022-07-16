e3378c0573d468f0cf19cad65a234067a1c4e578
Nahum Litvin <lnahum@gmail.com>
2022-07-16 11:32:38 +0300

    sat16th - basic solution

--- 226.invert-binary-tree/main.py
+++ 226.invert-binary-tree/main.py
@@ -1,16 +1,24 @@
-from operator import inv
-from typing import Optional
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+from functools import reduce
+
+
+from typing import *
 class Solution:
-    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if not root:
-            return None
-        tmp= self.invertTree(root.right)
-        root.right  = self.invertTree(root.left)
-        root.left = tmp
-        return root
-             
+    def addToDict(self,s:str):
+        dic: Dict[str,int]= {}
+        for i in s:
+            if i in dic:
+                dic[i]+=1
+            else:
+                dic[i]=1
+        return dic
+                
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        dic1 = self.addToDict(s)
+        dic2 = self.addToDict(t)
+        return dic1 == dic2
+            
+            
+sol = Solution()
+sol.isAnagram("rat","cat")
