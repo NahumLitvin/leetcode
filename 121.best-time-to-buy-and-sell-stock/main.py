82b972b779ae2e8dfc5735dbe271b9fe774e4418
Nahum Litvin <lnahum@gmail.com>
2022-07-16 09:52:25 +0300

    121.best-time - stil lfailing , missing edge case

--- 121.best-time-to-buy-and-sell-stock/main.py
+++ 121.best-time-to-buy-and-sell-stock/main.py
@@ -2,26 +2,21 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        min = prices[0]
-        max = prices[0]
-        min_candidate = prices[0] #becomes the new min if we find a complemental max
+        min = 1000
+        max = 0
+        min_candidate = 1000 #becomes the new min if we find a complemental max
         for price in prices:
+            print(f'{min} , {max} , {min_candidate}')
+            if price > max and price > min:
+                max = price
             if price < min:
                 min_candidate=price # this is the lowest price we saw
-            if price < max and  min - min_candidate >= max-price:
+            if price - min_candidate >= max - min:
                 max = price
                 min = min_candidate
-            if price > max:
-                max = price
         return max - min
                 
             
             
-            
-            
-            
-# 2  5  0  4
-
-min  2  2   
-max  2  5   
-cand 2  2   0
+sol = Solution()  
+print(sol.maxProfit([4,7,1,2,11]) )   
